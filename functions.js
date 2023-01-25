d067a50af0b9f2a658d62f2d1ef4440e84816535
JacksonRhea <97003378+JacksonRhea@users.noreply.github.com>
2023-01-24 20:05:15 -0700

    Overlay on all projects, need to finalize

--- functions.js
+++ functions.js
@@ -1,8 +1,40 @@
-function openOverlay() {
-  document.body.classList.add('activeOverlay');
+function openOverlay1() {
+  document.body.classList.add('activeOverlay1');
   document.body.classList.add('endScroll');
 }
 
-function closeOverlay() {
-  document.body.classList.remove('activeOverlay');
+function closeOverlay1() {
+  document.body.classList.remove('activeOverlay1');
+}
+
+function openOverlay2() {
+  document.body.classList.add('activeOverlay2');
+}
+
+function closeOverlay2() {
+  document.body.classList.remove('activeOverlay2');
+}
+
+function openOverlay3() {
+  document.body.classList.add('activeOverlay3');
+}
+
+function closeOverlay3() {
+  document.body.classList.remove('activeOverlay3');
+}
+
+function openOverlay4() {
+  document.body.classList.add('activeOverlay4');
+}
+
+function closeOverlay4() {
+  document.body.classList.remove('activeOverlay4');
+}
+
+function openOverlay5() {
+  document.body.classList.add('activeOverlay5');
+}
+
+function closeOverlay5() {
+  document.body.classList.remove('activeOverlay5');
 }
