fa7ca88e112ce5d5c697f5f27c8e0708d51b8c35
JacksonRhea <97003378+JacksonRhea@users.noreply.github.com>
2023-02-20 23:43:18 -0700

    New overlay, messing with blur and closing

--- functions.js
+++ functions.js
@@ -1,40 +1,7 @@
-function openOverlay1() {
-  document.body.classList.add('activeOverlay1');
-  document.body.classList.add('endScroll');
+function openOverlay() {
+  document.body.classList.add("active-popup")
 }
 
-function closeOverlay1() {
-  document.body.classList.remove('activeOverlay1');
-}
-
-function openOverlay2() {
-  document.body.classList.add('activeOverlay2');
-}
-
-function closeOverlay2() {
-  document.body.classList.remove('activeOverlay2');
-}
-
-function openOverlay3() {
-  document.body.classList.add('activeOverlay3');
-}
-
-function closeOverlay3() {
-  document.body.classList.remove('activeOverlay3');
-}
-
-function openOverlay4() {
-  document.body.classList.add('activeOverlay4');
-}
-
-function closeOverlay4() {
-  document.body.classList.remove('activeOverlay4');
-}
-
-function openOverlay5() {
-  document.body.classList.add('activeOverlay5');
-}
-
-function closeOverlay5() {
-  document.body.classList.remove('activeOverlay5');
+function closeOverlay() {
+  document.body.classList.remove("active-popup")
 }
